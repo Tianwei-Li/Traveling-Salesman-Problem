3b688d5b451fd6f515da42edebe47f4d60b72cbd
Tianwei Li <lee.tianwei@gmail.com>
2014-03-09 21:40:43 -0400

    add minimum spanning tree code

--- tsp.py
+++ tsp.py
@@ -7,6 +7,8 @@ import networkx as nx
 from sets import Set
 import heapq
 
+
+# data structure for tsp
 class NODE:
     id = 0
     g = 0.0    # maintain the current shortest path from 0 to this node
@@ -26,14 +28,61 @@ class NODE:
     def __cmp__(self, other):
         return cmp(self.f, other.f)
 
+# data structure for mst
+class NODE2:
+    id = 0
+    distToT = 0.0
+    def __init__(self, id):
+        self.id = id
 
-def mst(nodes):
+    def __del__(self):
+        pass
+
+    def __cmp__(self, other):
+        return cmp(self.distToT, other.distToT)
+
+
+def mst(nodes, G):
     if nodes[0] != 0:
         nodes.append(0)
-    #leftNodes.add(0)
-    #weight = 0.0
 
-    return 0.0
+    cost = 0.0
+
+    nodeCnt = len(nodes)
+
+    heap = []
+    leftNodes = {}
+    for i in range(nodeCnt):
+        id = nodes[i]
+        node = NODE2(id)
+        if id == 0:
+            node.distToT = float('-inf')
+        else:
+            node.distToT = float('inf')
+        heapq.heappush(heap, node)
+        leftNodes[id] = node
+    
+    while len(heap) != 0:
+        u = heapq.heappop(heap)
+        del leftNodes[u.id]
+        if u.id != 0:
+            cost += u.distToT
+        
+        u.distToT = float('-inf')
+        needHeapify = False
+        for idx in G.neighbors(u.id):
+            if leftNodes.has_key(idx):
+                v = leftNodes.get(idx)
+                dist = G[u.id][v.id]['weight']
+                if dist < v.distToT:
+                    v.distToT = dist
+                    needHeapify = True
+        
+        if needHeapify == True:
+            heapq.heapify(heap)
+
+
+    return cost
 
 
 def main(argv):
@@ -42,7 +91,7 @@ def main(argv):
     nodeCnt = len(nodes)
 
     # heuristic value for node 0
-    heur = mst(nodes)
+    heur = mst(nodes, G)
     
     # maintain the heap to find the next shortest node
     heap = []
@@ -69,11 +118,12 @@ def main(argv):
     while len(heap) != 0:
         
         if heap[0].id != 0:
-            heur = mst(leftNodes.keys())
+            heur = mst(leftNodes.keys(), G)
 
         u = heapq.heappop(heap)
         del leftNodes[u.id]
         path.append(u.id)
+        needHeapify = False
         for idx in G.neighbors(u.id):
             if leftNodes.has_key(idx):
                 v = leftNodes.get(idx)
@@ -81,7 +131,10 @@ def main(argv):
                 if u.g + dist < v.g:
                     v.g = u.g + dist
                     v.f = v.g + heur
-                    heapq.heapify(heap)
+                    needHeapify = True
+        
+        if needHeapify == True:
+            heapq.heapify(heap)
 
 
     #nodes = G.nodes()
@@ -89,7 +142,7 @@ def main(argv):
     Cost = 0.0
     print('Tour:'),
     for i in range(nodeCnt):
-        print(' '),
+        #print(' '),
         print(path[i]),
         if i != 0:
             Cost += G[path[i-1]][path[i]]['weight']
@@ -97,19 +150,9 @@ def main(argv):
     print('')
     Cost += G[path[nodeCnt-1]][0]['weight']
     
-    print('Cost: '),
+    print('Cost:'),
     print(Cost)
-    #print(nodes)
-    #print(edges)
+
     
-    #G2 = nx.Graph()
-    #G2.add_nodes_from([1,2,3])
-    #G2.add_edge(1, 2)
-    #G2.add_edge(1, 3)
-    #G2[1][2]['weight'] = 1.2
-    #G2[1][3]['weight'] = 1.3
-
-
-
 if __name__=='__main__':
     main(sys.argv)
